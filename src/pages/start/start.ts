e1b80e6f1ae024f6716fa952d435ca44c237e062
Diego Hernán Moreno Amaya <diegomorenoa@unisangil.edu.co>
2019-07-24 14:52:53 -0500

    Start page animated

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -6,16 +6,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-start',
   templateUrl: 'start.html',
 })
+
 export class StartPage {
 
+  @ViewChildren('playBtn', { read: ElementRef }) playBtn: QueryList<ElementRef>;
+  
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams
   ) {
   }
 
+  animateBtn() {
+    this.playBtn.map(el => {
+      // Remove delay
+      setTimeout(() => {
+        el.nativeElement.classList.remove('delay-2s');
+      }, 3000);
+      // Set Interval for 5s
+      setInterval(() => {
+        el.nativeElement.classList.remove('rubberBand');
+        setTimeout(() => {
+          el.nativeElement.classList.add('rubberBand');
+        }, 1000);
+      }, 5000);
+    });
+  }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StartPage');
+    console.log('ionViewDidLoad StartPage', this.playBtn);
+    this.animateBtn();
   }
 
 }
