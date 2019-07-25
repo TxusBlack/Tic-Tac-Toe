f7bca79dcbf33fd4fe8c384de76f6f34826f5a87
Diego Hernán Moreno Amaya <diegomorenoa@unisangil.edu.co>
2019-07-24 20:54:25 -0500

    Draw the game on the table and reset added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,8 @@ export class HomePage {
 
   public matrix: any;
   public x: number = 0;
-  public y: number = 0;
+  public o: number = 0;
+  private count: number = 0;
 
   constructor(
     public navCtrl: NavController
@@ -20,6 +21,30 @@ export class HomePage {
 
   makeMatrix(rows, columns, el) {
     return Array(rows).fill(el).map(() => Array(columns).fill(el))
+  }
+
+  set(row, col) {
+    if (this.x < 5 && !this.matrix[row][col]) {
+      const turn = this.count % 2;
+      if (!turn) {
+        this.matrix[row][col] = 'x';
+        this.x += 1;
+      } else {
+        this.matrix[row][col] = 'o';
+        this.o += 1;
+      }
+      this.checkWinner();
+      this.count += 1;
+    }
+  }
+
+  checkWinner() {
+
+  }
+
+  newGame() {
+    this.matrix = this.makeMatrix(3, 3, 0);
+    this.x = 0;
   }
 
   ionViewWillEnter() {
