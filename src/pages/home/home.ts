0e1e50d9e92b0e94d9e48d6f2b649ed96ad0a71f
Diego Hernán Moreno Amaya <diegomorenoa@unisangil.edu.co>
2019-07-24 19:54:44 -0500

    Frontend finished in the table of the player

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,10 +8,25 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public matrix: any;
+  public x: number = 0;
+  public y: number = 0;
+
+  public titleAnimated: string = '';
+
   constructor(
     public navCtrl: NavController
   ) {
+    
+  }
 
+  makeMatrix(rows, columns, el) {
+    return Array(rows).fill(el).map(() => Array(columns).fill(el))
+  }
+
+  ionViewWillEnter() {
+    this.matrix = this.makeMatrix(3, 3, 0);
+    this.titleAnimated = 'fadeIn';
   }
 
 }
